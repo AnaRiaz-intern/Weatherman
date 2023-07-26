ff9e79306e7bc5c17d5ed006560033ac1fe823c6
AnaRiaz-intern <anariaz.intern@devsinc.com>
2023-07-26 23:55:58 +0500

    monthly-findings

--- helper.py
+++ helper.py
@@ -1,9 +1,13 @@
+from colorama import Fore, init
+init()
+
+
 class Weather_man ():
     def __init__(self, date, highest_temp, lowest_temp, humidity):
         self.date = date
-        self.highest_temp = highest_temp
-        self.lowest_temp = lowest_temp
-        self.humidity = humidity
+        self.highest_temp = int(highest_temp) if highest_temp else 0
+        self.lowest_temp = int(lowest_temp) if lowest_temp else 0
+        self.humidity = int(humidity) if humidity else 0
 
     def get_date(self):
         return self.date
@@ -14,37 +18,138 @@ class Weather_man ():
     def get_lowest_temp(self):
         return self.lowest_temp
 
-    def get_get_humidity(self):        
+    def get_humidity(self):        
         return self.humidity
 
     def __str__(self):
         return f"Date: {self.date}, Highest Temp: {self.highest_temp},Lowest Temp: {self.lowest_temp}, Humidity: {self.humidity}"
 
 
+# def read_file(path):
+#     data = []
+#     with open(path, 'r') as file:
+#         first_line = file.readline().strip().split(',')
+#         if not first_line:
+#             file.readline()
+#         for line in file:
+#             data_values = line.strip().split(',')
+#             # date -> 0th index
+#             # highest temp -> 1st index
+#             # lowest temp -> 3rd inex
+#             # most humid day -> day with max humity -> 7th index
+#             required_entry = Weather_man(data_values[0], data_values[1],
+#                                          data_values[3], data_values[7])
+#             data.append(required_entry)
+#     return data
 def read_file(path):
     data = []
     with open(path, 'r') as file:
-        file.readline().strip().split(',')
+        first_line = file.readline().strip().split(',')
+        if len(first_line) < 8:  # Check if the first line has enough values
+            file.readline()
+
         for line in file:
             data_values = line.strip().split(',')
-            # date -> 0th index
-            # highest temp -> 1st index
-            # lowest temp -> 3rd inex
-            # most humid day -> day with max humity -> 7th index
-            required_entry = Weather_man(data_values[0], data_values[1],
-                                         data_values[3], data_values[7])
+            if len(data_values) < 8:  # Check if the line has enough values
+                continue
+
+            date = data_values[0]
+            highest_temp = data_values[1]
+            lowest_temp = data_values[3]
+            humidity = data_values[7]
+
+            # Handle missing values by setting them to 0
+            highest_temp = int(highest_temp) if highest_temp.strip() else 0
+            lowest_temp = int(lowest_temp) if lowest_temp.strip() else 0
+            humidity = int(humidity) if humidity.strip() else 0
+
+            required_entry = Weather_man(date, highest_temp, lowest_temp, humidity)
             data.append(required_entry)
+
     return data
 
 
-def find_highest_temperature_and_day(data):
+def Yearly_findings(data):
     if not data:
         print("No data available for this year :(\n")
     highest_temp = 0
-    date = None
+    lowest_temp = 0
+    high_humidity = 0
+    date1 = None
+    date2 = None
+    date3 = None
+    for entry in data:
+        temp1 = entry.get_highest_temp()
+        temp2 = entry.get_lowest_temp()
+        temp3 = entry.get_humidity()
+        if (highest_temp == 0) or (temp1 > highest_temp):
+            highest_temp = temp1
+            date1 = entry.get_date()
+        if (lowest_temp == 0) or (temp2 < lowest_temp):
+            lowest_temp = temp2
+            date2 = entry.get_date()
+        if (high_humidity == 0) or (temp3 > high_humidity):
+            high_humidity = temp3
+            date3 = entry.get_date()
+    return highest_temp, date1, lowest_temp, date2, high_humidity, date3
+
+
+def Monthly_Averages_Findings(data):
+    if not data:
+        print("No data available for this month :(\n")
+        return
+
+    count = len(data)
+    total_highest_temp = sum(entry.get_highest_temp() for entry in data)
+    total_lowest_temp = sum(entry.get_lowest_temp() for entry in data)
+    total_humidity = sum(entry.get_get_humidity() for entry in data)
+
+    average_highest_temp = total_highest_temp / count
+    average_lowest_temp = total_lowest_temp / count
+    average_humidity = total_humidity / count
+    print(f"Average Highest Temperature: {average_highest_temp:.2f}")
+    print(f"Average Lowest Temperature: {average_lowest_temp:.2f}")
+    print(f"Average Humidity: {average_humidity:.2f}")
+    print("----------------------------\n")
+
+
+# Function to draw the bar chart
+
+
+def Find_High_temp(data):
+    if not data:
+        print("No data available :(\n")
+    highest_temp = 0
+    # min temp on the day with highest temp
+    min_temp_on_hottest_day = 0
+    # max temp on the coldest day
+    high_temp_day = None
     for entry in data:
         temp = entry.get_highest_temp()
         if (highest_temp == 0) or (temp > highest_temp):
             highest_temp = temp
-            date = entry.get_date()
-    return highest_temp, date
+            high_temp_day = entry.get_date()
+            min_temp_on_hottest_day = entry.get_lowest_temp()
+    return high_temp_day, highest_temp, min_temp_on_hottest_day
+
+
+def Find_Low_temp(data):
+    if not data:
+        print("No data available :(\n")
+    lowest_temp = 0
+    # max temp on the coldest day
+    max_temp_on_coldest_day = 0
+    low_temp_day = None
+    for entry in data:
+        temp = entry.get_lowest_temp()
+        if (lowest_temp == 0) or (temp < lowest_temp):
+            lowest_temp = temp
+            low_temp_day = entry.get_date()
+            max_temp_on_coldest_day = entry.get_highest_temp()
+    return low_temp_day, lowest_temp, max_temp_on_coldest_day
+
+
+def print_horizontal_bar_chart(value, color_code):
+    for i in range(int(value)):
+        print(color_code + " +", end='')
+    print()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import helper
+import Calander
 import os
 import sys
 
@@ -8,17 +9,18 @@ n = len(sys.argv)
 print("Total arguments passed:", n)
 current_path = os.getcwd()
 problem_type = sys.argv[1]
-given_year = str(sys.argv[2])
 folder_name = sys.argv[3]
 folder_path = os.path.join(current_path, folder_name)
+
 print("\nYou selected this folder: ", folder_name, "\n")
 text_files = []
-rows = []
+for filename in os.listdir(folder_path):
+    if filename.endswith('.txt'):
+        text_files.append(os.path.join(folder_path, filename))
 
+rows = []
 if problem_type == "-e":
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.txt'):
-            text_files.append(os.path.join(folder_path, filename))
+    given_year = str(sys.argv[2])
     for file_name in text_files:
         if given_year in file_name:
             # store the data of filenames having that year in their
@@ -26,12 +28,49 @@ if problem_type == "-e":
             rows.append(helper.read_file(file_name))
     # we now have the desired rows
     highest_temperature = 0
+    lowest_temp = 0
+    high_humidity = 0
     highest_temperature_day = None
+    lowest_temp_day = None
+    high_humidity_day = None
     for row in rows:
-        current_ht, current_d = helper.find_highest_temperature_and_day(row)
+        current_ht, current_d, curr_lt, curr_lt_d, curr_hh, curr_hh_d = helper.Yearly_findings(row)
         if int(current_ht) > int(highest_temperature):
             highest_temperature = current_ht
             highest_temperature_day = current_d
-
+        if int(curr_lt) < int(lowest_temp):
+            lowest_temp = curr_lt
+            lowest_temp_day = curr_lt_d
+        if int(curr_hh) > int(high_humidity):
+            high_humidity = curr_hh
+            high_humidity_day = curr_hh_d
+    print("--------------------------------")
+    print("Here's Your yearly Report")
+    print("--------------------------------\n")
     print(f"Highest Temp: {highest_temperature} on {highest_temperature_day}")
+    print(f"Lowest Temp: {lowest_temp} on {lowest_temp_day}")
+    print(f"Highest Humidity: {high_humidity} on {high_humidity_day}")
+
+    print("--------------------------------\n")
     print("\n\n")
+
+if problem_type == "-a":
+    given_year, given_month = Calander.extract_month_and_year(sys.argv[2])
+    print("\n---------------------------------")
+    print("Here's Your Monthly Report")
+    print("---------------------------------")
+    print("Year:", given_year, " | Month:", given_month, "\n")
+    for file_name in text_files:
+        if given_month in file_name:
+            if given_year in file_name:
+                rows.append(helper.read_file(file_name))
+    for row in rows:
+        helper.Monthly_Findings(row)
+
+if problem_type == "-c":
+    given_year, given_month = Calander.extract_month_and_year(sys.argv[2])
+    for file_name in text_files:
+        if given_month in file_name and given_year in file_name:
+            rows.append(helper.read_file(file_name))
+
+
